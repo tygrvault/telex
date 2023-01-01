438c1e3924de9451fcce75a11d38999fc6223d7e
Ty Mason <tygerxqt@proton.me>
2023-01-01 00:50:52 +0000

    Give a type to the command

--- src/Commands/help.ts
+++ src/Commands/help.ts
@@ -4,6 +4,6 @@ export const command: BotCommand = {
     name: "help",
     description: "Shows this message",
     run: (ctx) => {
-        ctx.reply(`I can do the following things:\n/help - Shows this message\n/leave - I will leave the chat\n/echo - I will repeat what you say\n/quit - I will leave the chat (Admin)`);
+        ctx.reply(`I can do the following things:\n/help - Shows this message\n/echo - I will repeat what you say\n/quit - I will leave the chat (Admin)`);
     }
 }
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import { Context, Telegraf } from "npm:telegraf";
 import { Update } from "npm:typegram";
 import * as path from "https://deno.land/std@0.170.0/path/mod.ts";
 import { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
+import { BotCommand } from "./Interfaces/BotCommand.ts";
 
 config({ export: true });
 
@@ -9,7 +10,7 @@ const bot: Telegraf<Context<Update>> = new Telegraf(Deno.env.get("BOT_TOKEN") as
 const commandPath = path.join(new URL('.', import.meta.url).pathname, "/Commands").slice(1);
 
 for (const file of Deno.readDirSync(commandPath)) {
-    const { command } = await import(`./Commands/${file.name}`);
+    const { command }: { command: BotCommand } = await import(`./Commands/${file.name}`);
     console.log("Loaded command: " + command.name);
     bot.command(command.name, (ctx) => command.run(ctx));
 }
